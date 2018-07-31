e0267d1cc2fe2a9baf24f6bfa938d9549ca731f1
Can Toraman <cantoraman@gmail.com>
2018-07-31 15:59:38 +0100

    passes player cards test

---
+++ player.js
@@ -0,0 +1,5 @@
+const Player = function(name, cards) {
+  this.name = name;
+  this.cards = cards;
+}
+module.exports = Player;
--- specs/player_specs.js
+++ specs/player_specs.js
@@ -8,17 +8,30 @@ let card1;
 let card2;
 let card3;
 let card4;
-
+let cards;
 beforeEach(function(){
   card1 = new Card("Superman", 6, 9,7);
   card2 = new Card("Scarlet Witch", 7, 10,5);
   card3 = new Card("Black Widow", 8, 6, 9);
   card4 = new Card("Flash", 7,4,10);
+  cards= [card1,card2,card3,card4];
+  player = new Player("Can", cards);
 
-  player = new Player()
+});
+
+it("should have a name", function(){
+  const result = player.name;
+  assert.strictEqual(result, "Can");
+});
+
+it("should have cards", function(){
+  const result = player.cards;
+  assert.deepStrictEqual(result, cards);
+
 
 
 });
+
 
 
 });
